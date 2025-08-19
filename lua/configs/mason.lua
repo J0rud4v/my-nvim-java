ada0cb352aa76ed065d1f948181e10ee38d0ae58
j0rud4v <jorgets222@gmail.com>
2025-08-19 01:00:34 -0500

    LSPs y Complemntos necesarios agregados a ensure_installed

--- lua/configs/mason.lua
+++ lua/configs/mason.lua
@@ -1,5 +1,17 @@
 return {
-    ensure_installed = {'lua-language-server', 'stylua', 'bash-language-server', 'jdtls'},
+    ensure_installed = {
+        'lua-language-server',
+        'stylua',
+        'bash-language-server',
+        'jdtls',
+        'java-debug-adapter',
+        'java-test',
+        'lemminx',
+        'typescript-language-server',
+        'html-lsp',
+        'pyright',
+        'debugpy',
+    },
 
     max_concurrent_installers = 10,
 }
